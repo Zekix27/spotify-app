61b18bbaf757d2062c5ecaf1778f684c3a1c2992
Erwan <erwan.gerba@gmail.com>
2022-11-13 12:36:42 +0100

    update request url home

--- View/home.php
+++ View/home.php
@@ -1,6 +1,6 @@
 <?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.spotify.com/v1/artists");
+curl_setopt($ch, CURLOPT_URL, "https://api.spotify.com/v1/search?q=orelsan&type=artist");
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $_SESSION['token'] ));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
